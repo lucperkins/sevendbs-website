8b02e3395f93d2575b18a5f2deded65aeb0cb4b9
lucperkins <lucperkins@gmail.com>
2018-06-24 10:55:10 -0700

    new design

--- Makefile
+++ Makefile
@@ -1,2 +1,21 @@
+HUGO         = hugo
+NODE_BIN     = node_modules/.bin
+GULP         = $(NODE_BIN)/gulp
+CONCURRENTLY = $(NODE_BIN)/concurrently
+
+build-site:
+	$(HUGO)
+
+netlify-build: build-assets build-site
+
 serve:
-	hugo server --disableFastRender
+	hugo server --disableFastRender --ignoreCache
+
+develop-assets:
+	$(GULP) dev
+
+build-assets:
+	$(GULP) build
+
+dev: build-assets
+	$(CONCURRENTLY) "make develop-assets" "make serve"
--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,12 @@ var gulp     = require("gulp"),
     sass     = require("gulp-sass"),
     hash     = require("gulp-hash"),
     prefixer = require("gulp-autoprefixer"),
-    uglify   = require("gulp-uglify"),
     del      = require("del");
 
 var SRCS = {
   sass: ["source/sass/style.sass"],
   sassWatch: ["source/sass/**/*.sass"]
 }
-
-gulp.task('sass', (done) => {
-  gulp.src(SRCS.sass)
-    .pipe(sass({
-      outputStyle: 'compressed'
-    }))
-    .pipe(prefixer({
-      browsers: ['last 2 versions'],
-      cascade: false
-    }))
-    .pipe(gulp.dest('static/css'));
-
-  done();
-});
 
 gulp.task('sass-dev', (done) => {
   del(['static/css/style-*.css']);
@@ -47,27 +32,7 @@ gulp.task('sass:watch', () => {
   gulp.watch(SRCS.sassWatch, gulp.series('sass-dev'));
 });
 
-/*
-gulp.task('js', (done) => {
-  del(['static/js/app-*.js']);
-
-  gulp.src(SRCS.js)
-    .pipe(concat("app.js"))
-    .pipe(uglify())
-    .pipe(hash())
-    .pipe(gulp.dest('static/js'))
-    .pipe(hash.manifest('assetHashes.json'))
-    .pipe(gulp.dest('data'));
-
-  done();
-});
-
-gulp.task('js:watch', () => {
-  gulp.watch(SRCS.js, gulp.series('js'));
-});
-*/
-
-gulp.task('build', gulp.series('sass'));
+gulp.task('build', gulp.series('sass-dev'));
 
 gulp.task('dev', gulp.series('sass-dev', gulp.parallel('sass:watch')));
 
