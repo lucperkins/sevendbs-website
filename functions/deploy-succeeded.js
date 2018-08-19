fca7c89e6326809b7e221e0c1ebc0ee513177f64
lucperkins <lucperkins@gmail.com>
2018-08-19 13:50:02 -0700

    Add error handling logic

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,16 +1,27 @@
-const https = require('https');
+const https = require('https'),
+      url = 'https://7dbs.io';
 
-const handle = (res) => {
+const handleResponse = (res) => {
   const headers = res.headers;
 
-  Object.keys(headers).forEach((key) => {
-    const header = headers[key];
-    console.log("Header: " + header);
-  });
+  if ("X-Robots-Tag" in headers) {
+    callback(null, {
+      statusCode: 500,
+      body: `The page ${url} contains the "X-Robots-Tag" header`
+    });
+
+  } else {
+    callback(null, {
+      statusCode: 200,
+      body: "All clear!"
+    })
+  }
+}
+
+const handleError = (e) => {
+  console.error(e);
 }
 
 exports.handler = (event, context, callback) => {
-  console.log("Processing function...");
-
-  https.get("https://7dbs.io", handle);
+  https.get(url, handleResponse).on('error', handleError);
 }
