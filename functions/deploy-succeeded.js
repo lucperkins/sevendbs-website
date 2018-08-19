15872a365add7390458c5b55f44c13e7216453de
lucperkins <lucperkins@gmail.com>
2018-08-19 13:43:42 -0700

    Add header fetching to function

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,3 +1,15 @@
-exports.handler = function(event, context, callback) {
+const https = require('https');
+
+const handle = (res) => {
+  const headers = res.headers;
+
+  headers.forEach((header) => {
+    console.log(header);
+  });
+}
+
+exports.handler = (event, context, callback) => {
   console.log("Processing function...");
+
+  https.get("https://7dbs.io", handle);
 }
