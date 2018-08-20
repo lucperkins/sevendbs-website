62a60dba6ae255c97ca5012aceb42aa6889abd50
lucperkins <lucperkins@gmail.com>
2018-08-20 15:21:51 -0700

    Add basic Slack message poster

--- functions/deploy-succeeded.js
+++ functions/deploy-succeeded.js
@@ -1,7 +1,27 @@
 const https = require('https'),
       slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
 
-
 exports.handler = (event, context, callback) => {
-  console.log(`Slack webhook URL: ${slackWebhookUrl}`);
+  const slackMessage = {
+    text: "This is an example message"
+  }
+
+  const httpOptions = {
+    hostname: slackWebhookUrl,
+    port: 80,
+    path: '/',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }
+
+  const req = https.request(httpOptions, (res) => {
+    res.on('data', (data) => console.log(`Data: ${data}`));
+    res.on('end', () => console.log('No more data in response'));
+  });
+
+  req.on('error', (e) => console.log(e));
+  req.write(slackMessage);
+  req.end();
 }
