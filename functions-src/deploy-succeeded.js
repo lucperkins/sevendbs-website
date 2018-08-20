c19306f07a35cb03216fb74ef81d90ebb47fc341
lucperkins <lucperkins@gmail.com>
2018-08-20 15:47:54 -0700

    Add notification

--- functions-src/deploy-succeeded.js
+++ functions-src/deploy-succeeded.js
@@ -1,15 +1,30 @@
-const { IncomingWebhook } = require('@slack/client');
-const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL || '';
+const { IncomingWebhook } = require('@slack/client'),
+      fetch = require('node-fetch';
+
+const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
 const webhook = new IncomingWebhook(slackWebhookUrl);
 
-exports.handler = (event, context, callback) => {
-  console.log(`Slack webhook URL: ${slackWebhookUrl}`);
+const sendSlackErrorMessage = (msg) => {
+  const slackMessageObject = {
+    text: msg
+  }
 
-  webhook.send('Here is a test message', (err, res) => {
+  webhook.send(slackMessageObject, (err, res) => {
     if (err) {
-      console.error(err);
+      console.error(`Slack webhook error: ${err}`);
     } else {
-      console.log(res);
+      console.log(`Response received from Slack: ${res}`);
     }
   });
+}
+
+exports.handler = (event, context, callback) => {
+  fetch('https://7dbs.io')
+    .then(res => {
+      const headers = res.headers;
+      if ('X-Robots-Tag' in headers) {
+        const msg = `WARNING: X-Robots-Tag found in main page headers.\n\nCurrent headers: ${JSON.stringify(headers)}`;
+        sendSlackErrorMessage(msg);
+      }
+    });
 }
