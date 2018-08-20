64a15ecf8f6bd151b931f11d65443c4c3dbb143d
lucperkins <lucperkins@gmail.com>
2018-08-20 15:39:39 -0700

    Remove env var check for now

--- functions-src/deploy-succeeded.js
+++ functions-src/deploy-succeeded.js
@@ -1,15 +1,10 @@
 const { IncomingWebhook } = require('@slack/client');
 const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL || '';
-
-if (!slackWebhookUrl) {
-  console.log("You must specify a Slack webhook URL using the SLACK_WEBHOOK_URL environment variable");
-  process.exitCode = 1;
-  return;
-}
-
 const webhook = new IncomingWebhook(slackWebhookUrl);
 
 exports.handler = (event, context, callback) => {
+  console.log(`Slack webhook URL: ${slackWebhookUrl}`);
+
   webhook.send('Here is a test message', (err, res) => {
     if (err) {
       console.error(err);
